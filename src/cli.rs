ff2a85ab49b196472d4fad7769f7ec785348abfe
asenetcky <asenetcky@gmail.com>
2024-10-01 09:35:39 -0400

    cleanup

--- src/cli.rs
+++ src/cli.rs
@@ -64,78 +64,8 @@ pub fn run(args: Args) -> Result<()> {
             CsvWriter::new(&mut file).finish(&mut data.df)?;
         }
         OutType::Stdout => {
-            // data.df.write_csv(output.file_name.unwrap())?;
             println!("{:?}", data.df);
         }
     }
-
-    // let mut out_file: Box<dyn Write> = match output.out_type {
-    //     OutType::Arrow => Box::new(File::create(output.file_name.unwrap())?),
-    //     OutType::Csv => Box::new(File::create(output.file_name.unwrap())?),
-    //     OutType::Stdout => Box::new(io::stdout()),
-    // };
-
-    // let mut print = |num: u64, text: &str| -> Result<()> {
-    //     if num > 0 {
-    //         if args.count {
-    //             write!(out_file, "{num:>4} {text}")?;
-    //         } else {
-    //             write!(out_file, "{text}")?;
-    //         }
-    //     };
-    //     Ok(())
-    // };
-
     Ok(())
 }
-
-// fn run(args: Args) -> Result<()> {
-//     let mut file = open(&args.in_file).map_err(|e| anyhow!("{}: {e}", args.in_file))?;
-//
-//     let mut out_file: Box<dyn Write> = match &args.out_file {
-//         Some(out_name) => Box::new(File::create(out_name)?),
-//         _ => Box::new(io::stdout()),
-//     };
-//
-//     let mut print = |num: u64, text: &str| -> Result<()> {
-//         if num > 0 {
-//             if args.count {
-//                 write!(out_file, "{num:>4} {text}")?;
-//             } else {
-//                 write!(out_file, "{text}")?;
-//             }
-//         };
-//         Ok(())
-//     };
-//
-//     let mut line = String::new();
-//     let mut previous = String::new();
-//     let mut count: u64 = 0;
-//
-//     loop {
-//         let bytes = file.read_line(&mut line)?;
-//         if bytes == 0 {
-//             break;
-//         }
-//
-//         if line.trim_end() != previous.trim_end() {
-//             print(count, &previous)?;
-//             previous = line.clone();
-//             count = 0;
-//         }
-//
-//         count += 1;
-//         line.clear();
-//     }
-//
-//     print(count, &previous)?;
-//
-//     Ok(())
-// }
-//
-// fn open(filename: &str) -> Result<Box<dyn BufRead>> {
-//     match filename {
-//         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
-//         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
-//     }
-// }
